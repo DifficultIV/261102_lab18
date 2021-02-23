a259d91ba00da6d1a9116b1dadac1d1c1c325e5d
DifficultIV <kitpisan@gmail.com>
2021-02-23 17:01:52 +0700

    done

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *a,Rect *b){
+	double h;
+	double w;
+	h = min((*a).y,(*b).y)-max((*a).y-(*a).h,(*b).y-(*b).h);
+	w = min((*a).x+(*a).w,(*b).x+(*b).w)-max((*a).x,(*b).x);
+	if(h > 0 && w > 0) return h*w;
+	else return 0;
 }
